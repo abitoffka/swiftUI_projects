0b9e7451105a225f0b03df0526621a87203de238
abitoffka <96001315+abitoffka@users.noreply.github.com>
2023-06-01 01:04:04 +0600

    Add files via upload

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Aigerim Abitayeva on 27.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
